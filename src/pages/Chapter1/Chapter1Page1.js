af6d4933f1c07c91092dbbde7e1a9d10e81993e9
Paweł Wicher <pwi@epcon.pl>
2020-04-25 09:37:25 +0200

    p monospace

--- src/pages/Chapter1/Chapter1Page1.js
+++ src/pages/Chapter1/Chapter1Page1.js
@@ -10,7 +10,14 @@ let xs = [1; 2; 3]
   return (
     <>
       <h1 className="display-4">Chapter 1 - Page 1</h1>
-      <p className="lead">
+      <br/>
+      <p class="lead">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+      incididunt ut labore et dolore magna aliqua. Diam quam nulla porttitor massa. 
+      Sit amet aliquam id diam maecenas ultricies mi eget. In nisl nisi scelerisque 
+      eu ultrices vitae auctor eu augue. Neque sodales ut etiam sit amet nisl.
+      </p>
+      <p class="text-monospace">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
       incididunt ut labore et dolore magna aliqua. Diam quam nulla porttitor massa. 
       Sit amet aliquam id diam maecenas ultricies mi eget. In nisl nisi scelerisque 
